8aca68064ab30f12038a8b91b32317c872862dca
Steven Goff <51921135+sgoff0@users.noreply.github.com>
2020-08-27 22:03:56 -0500

    Server controller to ts class

--- src/server-controller.ts
+++ src/server-controller.ts
@@ -13,98 +13,92 @@ import * as Logger from 'testarmada-midway-logger';
 import { argv as Argv } from './utils/configuration-parameters';
 import Constants from './constants';
 import ReadMockDataFromFile from './file-handler/file-handler';
-// TODO sgoff0 fix me as i'm exported strangely
-const internals = {
-  start: undefined,
-  addServerRoutesAndSessions: undefined,
-  stop: undefined,
-};
 
-internals.start = function (startOptions, callback) {
-  const midwayOptions = startOptions || {};
-  const DEFAULT_MOCK_DIRECTORY = Path.join(process.cwd(), Constants.MIDWAY_DEFAULT_MOCKED_DATA_LOC);
+class ServerController {
 
-  midwayOptions.startTime = new Date();
-  midwayOptions.port = Argv.midwayPort || midwayOptions.port || 8080;
-  midwayOptions.httpsPort = Argv.midwayHttpsPort || midwayOptions.httpsPort;
-  midwayOptions.host = (Argv.midwayHost || midwayOptions.host || 'localhost').replace(/.*?:\/\//g, '');
-  midwayOptions.project = midwayOptions.project || Constants.DEFAULT;
-  midwayOptions.proxyPort = Argv.proxyPort || midwayOptions.proxyPort;
-  midwayOptions.proxyHost = Argv.proxyHost || midwayOptions.proxyHost || 'localhost';
-  midwayOptions.mockedDirectory = Argv.mockedData || midwayOptions.mockedDirectory || DEFAULT_MOCK_DIRECTORY;
-  midwayOptions.sessions = Argv.midwaySessions || midwayOptions.sessions || 0;
-  midwayOptions.resolvedPath = MidwayUtils.checkDirectoryExists(midwayOptions.mockedDirectory) ? midwayOptions.mockedDirectory : DEFAULT_MOCK_DIRECTORY;
-  midwayOptions.respondWithFileHandler = ReadMockDataFromFile(midwayOptions.resolvedPath);
+  public start = (startOptions, callback) => {
+    const midwayOptions = startOptions || {};
+    const DEFAULT_MOCK_DIRECTORY = Path.join(process.cwd(), Constants.MIDWAY_DEFAULT_MOCKED_DATA_LOC);
 
-  if (midwayOptions.collectMetrics === undefined) {
-    midwayOptions.collectMetrics = true;
-  }
+    midwayOptions.startTime = new Date();
+    midwayOptions.port = Argv.midwayPort || midwayOptions.port || 8080;
+    midwayOptions.httpsPort = Argv.midwayHttpsPort || midwayOptions.httpsPort;
+    midwayOptions.host = (Argv.midwayHost || midwayOptions.host || 'localhost').replace(/.*?:\/\//g, '');
+    midwayOptions.project = midwayOptions.project || Constants.DEFAULT;
+    midwayOptions.proxyPort = Argv.proxyPort || midwayOptions.proxyPort;
+    midwayOptions.proxyHost = Argv.proxyHost || midwayOptions.proxyHost || 'localhost';
+    midwayOptions.mockedDirectory = Argv.mockedData || midwayOptions.mockedDirectory || DEFAULT_MOCK_DIRECTORY;
+    midwayOptions.sessions = Argv.midwaySessions || midwayOptions.sessions || 0;
+    midwayOptions.resolvedPath = MidwayUtils.checkDirectoryExists(midwayOptions.mockedDirectory) ? midwayOptions.mockedDirectory : DEFAULT_MOCK_DIRECTORY;
+    midwayOptions.respondWithFileHandler = ReadMockDataFromFile(midwayOptions.resolvedPath);
 
-  Logger.info('Starting midway server on http at http://' + midwayOptions.host + ':' + midwayOptions.port + '/midway');
-  if (midwayOptions.httpsPort) {
-    Logger.info('Starting midway server on https at https://' + midwayOptions.host + ':' + midwayOptions.httpsPort + '/midway');
-  }
+    if (midwayOptions.collectMetrics === undefined) {
+      midwayOptions.collectMetrics = true;
+    }
 
-  createHapiServer(midwayOptions, function (server) {
-    internals.addServerRoutesAndSessions(midwayOptions, server);
-    MidwayUtils.initFileHandler(midwayOptions.respondWithFileHandler);
+    Logger.info('Starting midway server on http at http://' + midwayOptions.host + ':' + midwayOptions.port + '/midway');
+    if (midwayOptions.httpsPort) {
+      Logger.info('Starting midway server on https at https://' + midwayOptions.host + ':' + midwayOptions.httpsPort + '/midway');
+    }
 
-    MidwayPluginController.runHapiWithPlugins(server, midwayOptions, callback);
-  });
-};
+    createHapiServer(midwayOptions, (server) => {
+      this.addServerRoutesAndSessions(midwayOptions, server);
+      MidwayUtils.initFileHandler(midwayOptions.respondWithFileHandler);
 
-internals.stop = function (server, callback) {
-  MidwayUtils.setServerRunningStatus(false);
-  const options = { timeout: 0 };
-  server.stop(options, function (err) {
-    if (err) {
+      MidwayPluginController.runHapiWithPlugins(server, midwayOptions, callback);
+    });
+  };
+
+  public stop = (server, callback) => {
+    MidwayUtils.setServerRunningStatus(false);
+    const options = { timeout: 0 };
+    server.stop(options, (err) => {
+      if (err) {
+        if (callback) {
+          return callback(err);
+        }
+      }
+      Logger.debug('Midway server stopped');
       if (callback) {
-        return callback(err);
+        return callback();
       }
-    }
-    Logger.debug('Midway server stopped');
-    if (callback) {
-      return callback();
-    }
-  });
-};
+    });
+  };
 
-internals.addServerRoutesAndSessions = function (midwayOptions, server) {
-  if (!MidwayUtils.isServerRunning()) {
-    MidwayUtils.setServerRunningStatus(true);
-    MidwayUtils.setServerProperties(midwayOptions);
+  public addServerRoutesAndSessions = (midwayOptions, server) => {
+    if (!MidwayUtils.isServerRunning()) {
+      MidwayUtils.setServerRunningStatus(true);
+      MidwayUtils.setServerProperties(midwayOptions);
 
-    // Add midway server apis and plugins
-    MidwayServerRoutes.addMidwayServerAPIs();
-    Logger.debug('Sessions to add: ' + midwayOptions.sessions);
+      // Add midway server apis and plugins
+      MidwayServerRoutes.addMidwayServerAPIs();
+      Logger.debug('Sessions to add: ' + midwayOptions.sessions);
 
-    // Intercept the response here using responseHandler
-    if (server) {
-      server.ext('onPostHandler', responseHandler);
-    }
-
-    // TODO this needs to be refactored and requestHandler should be generic
-    // TODO a request handler specific to session should be added and named differently
-    if (midwayOptions.sessions) {
-      // Intercept the request and response here
+      // Intercept the response here using responseHandler
       if (server) {
-        server.ext('onRequest', requestHandler);
+        server.ext('onPostHandler', responseHandler);
       }
-      // add midway session routes
-      MidwayServerRoutes.addRoutesToSessions(midwayOptions);
+
+      // TODO this needs to be refactored and requestHandler should be generic
+      // TODO a request handler specific to session should be added and named differently
+      if (midwayOptions.sessions) {
+        // Intercept the request and response here
+        if (server) {
+          server.ext('onRequest', requestHandler);
+        }
+        // add midway session routes
+        MidwayServerRoutes.addRoutesToSessions(midwayOptions);
+      }
+
+      // Initialize URL Call Counts
+      MidwayUtils.initializeSessionURLCallCount();
     }
+  };
 
-    // Initialize URL Call Counts
-    MidwayUtils.initializeSessionURLCallCount();
-  }
-};
-
+}
 function createHapiServer(midwayOptions, callback) {
   const server = new Hapi.Server();
   server.connection({ port: midwayOptions.port, labels: 'http' });
   return callback(server);
 }
-
-
-// module.exports = internals;
-export default internals;
+export default new ServerController();
