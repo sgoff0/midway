d2dfc8a74bde8399a46ebebc6e9caef226865334
Steven Goff <51921135+sgoff0@users.noreply.github.com>
2020-08-27 19:12:27 -0500

    Update midway system routes

--- src/server-routes/midway-system-routes.ts
+++ src/server-routes/midway-system-routes.ts
@@ -1,16 +1,3 @@
-/**
-* MIT License
-*
-* Copyright (c) 2018-present, Walmart Inc.,
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*
-*/
-// make this file a module
 import Utils from './../utils/common-utils';
 import SessionManager from './../session-manager/session-manager';
 import SessionInfo from './../session-manager/session-info';
@@ -20,8 +7,7 @@ const config = {
   tags: ['api']
 };
 
-export default {
-
+const routes = {
   setMockId: {
     id: 'Midway-SetMockId',
     label: 'Midway - Set Mock Id',
@@ -214,3 +200,4 @@ function getSessionIdFromRequest(req) {
   Logger.debug('Setting session id to ' + sessionId);
   return sessionId;
 }
+export default routes;
